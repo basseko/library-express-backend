853133edefe9523909c1dc010d482aabd4465302
Bassel Koshak <basselkoshak@Bassels-Macbook-Pro.local>
2024-12-14 20:31:09 +0300

    feat: add rate limiting support

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,23 @@ import morgan from "morgan";
 import bookRoutes from "./routes/bookRoutes";
 import userRoutes from "./routes/userRoutes";
 import { swaggerDocs } from "./swagger";
+import rateLimit from "express-rate-limit";
 
 const app: Application = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(compression());
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 60,
+  message: "Too many requests from this IP, please try again after 1 minute",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
